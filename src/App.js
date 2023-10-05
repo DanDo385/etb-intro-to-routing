f9bd8ad162accb7a49bb8d0065021435124cdb4a
Daniel Magro <dando385@yahoo.com>
2023-10-05 10:01:27 -0400

    2nd video complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import { Router } from 'react-router-dom';
+import { Router, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
    <Router>
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>  
+    
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
-   </Router>
+  </Router>
+  );
 }
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;  
+}
 export default App;
